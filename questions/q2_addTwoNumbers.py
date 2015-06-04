db55ea3fc444efe869cc23fdcdcfa2276f22583b
liangk12 <kenneth.liang12@gmail.com>
2015-06-03 23:49:24 -0400

    got solution for q1 add two numbers in single linked list with carry; need to dive more into code to understand the .next handling

--- questions/q2_addTwoNumbers.py
+++ questions/q2_addTwoNumbers.py
@@ -1,11 +1,3 @@
-"""
-You are given two linked lists representing two non-negative numbers.
-The digits are stored in reverse order and each of their nodes contain a single digit.
-Add the two numbers and return it as a linked list.
-
-Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-Output: 7 -> 0 -> 8
-"""
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -17,4 +9,17 @@ class Solution:
     # @param {ListNode} l2
     # @return {ListNode}
     def addTwoNumbers(self, l1, l2):
-        
+        carry = 0
+        root = temp = ListNode(0)
+        while l1 or l2 or carry:
+            tempSum=(l1.val if l1 else 0)+(l2.val if l2 else 0)+carry
+            temp.next = ListNode(tempSum % 10) # remainder, ones digit
+            temp = temp.next
+            carry = tempSum // 10 # get carry val (either 0 or 1)
+            if l1:
+                l1=l1.next
+            if l2:
+                l2=l2.next
+        return root.next
+
+# ref: https://leetcode.com/discuss/29326/a-python-solution
