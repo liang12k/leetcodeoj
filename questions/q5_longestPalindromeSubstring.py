b061154b35c9d3f1793f88f5a7ab56dcd9434043
liangk12 <kenneth.liang12@gmail.com>
2015-06-14 16:54:51 -0400

    stepping thru q5 longest palindrome string and adding comments for self reference and clarity

--- questions/q5_longestPalindromeSubstring.py
+++ questions/q5_longestPalindromeSubstring.py
@@ -40,19 +40,19 @@ O(n^2) solution I mentioned.
 class Solution:
     # @return a string
     def longestPalindrome(self, s):
-        if len(s)==0:
-            return 0
-        maxLen=1
-        start=0
+        if len(s)==0: return 0
+        maxLen,start=1,0
         for i in xrange(1,len(s)):
             if i-maxLen >=1 and s[i-maxLen-1:i+1]==s[i-maxLen-1:i+1][::-1]:
                 start=i-maxLen-1
                 maxLen+=2
+                # continue iteration because this is a longer string
+                # no need to check its interior elems as this is valid already
                 continue
-
             if i-maxLen >=0 and s[i-maxLen:i+1]==s[i-maxLen:i+1][::-1]:
                 start=i-maxLen
                 maxLen+=1
+        # return the palindrome substring of max length
         return s[start:start+maxLen]
 
 
