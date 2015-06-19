2875ee4865c6959b815c9beed6bd81a7aacace64
liangk12 <kenneth.liang12@gmail.com>
2015-06-19 00:11:51 -0400

    some ideas comments into q6 zigzag string

--- questions/q6_zigzagconversion.py
+++ questions/q6_zigzagconversion.py
@@ -3,4 +3,8 @@ class Solution:
     # @param {integer} numRows
     # @return {string}
     def convert(self, s, numRows):
-        
+        # ideas:
+        # numRows for number of lists to hold values
+        # idx to keep vals within list
+        # idx to keep count where char is entered
+        # whitespaces, TBD (some math to coordinate to idxs)
