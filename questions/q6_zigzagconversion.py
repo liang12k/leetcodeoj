26348d75dd8368fb5a1d338d7ab1e82518c300c3
liangk12 <kenneth.liang12@gmail.com>
2015-06-19 22:28:45 -0400

    addt'l comments into q6 zigzag question

--- questions/q6_zigzagconversion.py
+++ questions/q6_zigzagconversion.py
@@ -3,8 +3,13 @@ class Solution:
     # @param {integer} numRows
     # @return {string}
     def convert(self, s, numRows):
+        # note: the direction is |/|/|/...|
         # ideas:
+        # O(n) go thru whole 's' in single pass
+        # need to handle base case of few chars (w applying the logic below)
         # numRows for number of lists to hold values
         # idx to keep vals within list
         # idx to keep count where char is entered
-        # whitespaces, TBD (some math to coordinate to idxs)
+        # 
+        # whitespaces, TBD (are they needed?)
+        # -'ignore' them and just add in the char at correct positions
