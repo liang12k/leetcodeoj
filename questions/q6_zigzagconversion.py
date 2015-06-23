ec1f3464740eb5ce9b45150479c759b14f02167d
liangk12 <kenneth.liang12@gmail.com>
2015-06-22 21:38:22 -0400

    attempting q6 zigzag - error in logic when inserting char into numRows list; figure it out

--- questions/q6_zigzagconversion.py
+++ questions/q6_zigzagconversion.py
@@ -14,3 +14,25 @@ class Solution:
         # 
         # whitespaces, TBD (are they needed?)
         # -'ignore' them and just add in the char at correct positions
+        def convert(self, s, numRows):
+            if numRows==1 or numRows>len(s): return s
+            rowslist=range(numRows)
+            d={}
+            for idx,ch in enumerate(s):
+                coln=0
+                if idx%numRows==0 and idx!=0:
+                    coln+=1
+                if coln not in [0,numRows-1]:
+                    d[rowslist[0-(idx%numRows)-2]]=d.get(rowslist[0-(idx%numRows)-2],[])+[ch]
+                else:
+                    d[idx%numRows]=d.get(idx%numRows,[])+[ch]
+            s=""
+            for _ in xrange(numRows):
+                s+="".join([ch for ch in d.get(_,"") if ch])
+            return s
+
+
+if __name__=="__main__":
+    print Solution().convert("ABCDE",3)
+    # giving "ABDEC", should be "AEBDC" (passed 219/1158)
+    # error somewhere in inserting chars
