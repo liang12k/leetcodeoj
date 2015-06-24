18308c56a3a58604843dd69e9d23f8d9ce29f8d4
liangk12 <kenneth.liang12@gmail.com>
2015-06-23 22:22:19 -0400

    got solution for q6 zigzag at 160ms w/o all the comments. Needs to be faster (different implementation approach).

--- questions/q6_zigzagconversion.py
+++ questions/q6_zigzagconversion.py
@@ -1,38 +1,44 @@
+## solution is ~ @ 160ms, needs to be faster
 class Solution:
     # @param {string} s
     # @param {integer} numRows
     # @return {string}
     def convert(self, s, numRows):
         # note: the direction is |/|/|/...|/|
-        # 
-        # ideas:
-        # O(n) go thru whole 's' in single pass
-        # need to handle base case of few chars (w applying the logic below)
-        # numRows for number of lists to hold values
-        # idx to keep vals within list
-        # idx to keep count where char is entered
-        # 
-        # whitespaces, TBD (are they needed?)
-        # -'ignore' them and just add in the char at correct positions
-        def convert(self, s, numRows):
-            if numRows==1 or numRows>len(s): return s
-            rowslist=range(numRows)
-            d={}
-            for idx,ch in enumerate(s):
-                coln=0
-                if idx%numRows==0 and idx!=0:
-                    coln+=1
-                if coln not in [0,numRows-1]:
-                    d[rowslist[0-(idx%numRows)-2]]=d.get(rowslist[0-(idx%numRows)-2],[])+[ch]
-                else:
-                    d[idx%numRows]=d.get(idx%numRows,[])+[ch]
-            s=""
-            for _ in xrange(numRows):
-                s+="".join([ch for ch in d.get(_,"") if ch])
-            return s
-
-
-if __name__=="__main__":
-    print Solution().convert("ABCDE",3)
-    # giving "ABDEC", should be "AEBDC" (passed 219/1158)
-    # error somewhere in inserting chars
+        # return str s as is for base cases
+        if numRows==1 or numRows>=len(s): return s
+        initrow=0
+        endcol=True
+        d={}
+        # iterate O(n) thru str s
+        for ch in s:
+            # append ch into proper row
+            d[initrow]=d.get(initrow,[])+[ch]
+            # increment initrow if on 0 or (coln-1)%nRows==0
+            # as this means the chars go downward
+            # decrement initrow if not on 0 or (coln-1)%nRows==0
+            # as these are the diagonals
+            initrow=initrow+(1 if endcol else -1)
+            if initrow==numRows:
+                # after finish incrementing over rows
+                # go back to the diagonal position for decrementing
+                # eg: ABCD, 3
+                # 0 A
+                # 1 B D <-- 'D' went over and is in the diagonal
+                # 2 C
+                initrow-=2
+                endcol=False
+            elif initrow==-1:
+                # after finish decrementing over diagonal
+                # go back to the 0 row position for incrementing
+                # eg: ABCDE, 3
+                # 0 A   E <-- 'E' went over and is in the new row 0, col0
+                # 1 B D
+                # 2 C
+                initrow+=2
+                endcol=True
+        # concat all list values of chars for return output
+        s=""
+        for _ in xrange(numRows):
+            s+="".join(d.get(_,""))
+        return s
