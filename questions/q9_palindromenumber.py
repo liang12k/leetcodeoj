5c4b1bce26b08a8e2f417b314282017fe012c07e
liangk12 <kenneth.liang12@gmail.com>
2015-07-01 21:46:45 -0400

    completed q9 palindrome number - comments added

--- questions/q9_palindromenumber.py
+++ questions/q9_palindromenumber.py
@@ -16,17 +16,12 @@ class Solution(object):
     # @param {integer} x
     # @return {boolean}
     def isPalindrome(self, x):
-        # perhaps a while loop - while(isPalindromeFlag): ... break otherwise
-        if x==0: return True
         x=str(x)
-        for idx,n in enumerate(x):
-            if x[idx]!=x[0-idx-1]: break
-        if x[0]=="-": return False
-        x=int(x)
-        if x>=2147483647 or x<=-2147483648:
-            x=0
-        return bool(x)
-
-
-if __name__ == '__main__':
-    print Solution().isPalindrome(10) # expected False, returning True (because of bool(x))
+        # iterate thru entire str of nums
+        for idx in xrange(len(x)):
+            # check from front and back values towards center
+            # this handles detecting the '-' sign as neg nums aren't palindromes
+            if x[idx]!=x[0-idx-1]: return False
+        # as all values meet palindrome requirement
+        # bool if x doesn't surpass int max value for overflow
+        return not int(x)>=2147483647
