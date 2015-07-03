331b9ee854cd8abebed52ccd2e9f8f9ebb0ec7ee
liangk12 <kenneth.liang12@gmail.com>
2015-07-03 17:44:56 -0400

    added comments for q12 int to roman numerals

--- questions/q12_integerToRoman.py
+++ questions/q12_integerToRoman.py
@@ -8,13 +8,19 @@ class Solution(object):
     # @return {string}
     def intToRoman(self, num):
         romanstr=""
+        # list of standard int and their respective roman numeral str
+        # important to handle the special numbers before a new roman numeral
+        # the leading 9, 4 numbers - eg: 900 (CM), 40 (XL)
+        # order from larges to least to decrement num from top down (left to right)
         rmnums=[
             (1000, 'M'), (900, 'CM'), (500, 'D'),  (400, 'CD'), (100, 'C'), (90, 'XC'),
             (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
         ]
         for pair in rmnums:
             n,r=pair
+            # continue decrementing num with current n value from rmnums list
+            # exit and go to next until num has finished decrementing
             while num-n>-1:
-                romanstr+=r
-                num-=n
+                romanstr+=r # concat roman numeral str
+                num-=n      # decrement num using current n
         return romanstr
