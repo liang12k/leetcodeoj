178f8212bbd97f199b3ad10c9429b1f25e3bb5a4
liangk12 <kenneth.liang12@gmail.com>
2015-07-05 21:33:00 -0400

    placeholder for q14 longest common prefix str

---
+++ questions/q14_longestCommonPrefix.py
@@ -0,0 +1,8 @@
+'''
+Write a function to find the longest common prefix string
+amongst an array of strings.
+'''
+class Solution(object):
+    # @param {string[]} strs
+    # @return {string}
+    def longestCommonPrefix(self, strs):
