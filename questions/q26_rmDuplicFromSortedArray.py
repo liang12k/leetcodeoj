3fd0e32102328022ed67598e461ae0128c23f9a5
liangk12 <kenneth.liang12@gmail.com>
2015-07-09 21:26:02 -0400

    working on q26 remove duplicates from sorted array; constant space

---
+++ questions/q26_rmDuplicFromSortedArray.py
@@ -0,0 +1,28 @@
+'''
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+It doesn't matter what you leave beyond the new length.
+'''
+class Solution(object):
+    # @param {integer[]} nums
+    # @return {integer}
+    def removeDuplicates(self, nums):
+        if not nums: return 0
+        currn=None
+        counter=0
+        for n in nums:
+            if currn!=n:
+                currn=n
+                counter+=1
+        return range(counter)
+
+
+if __name__=="__main__":
+	inp=[1,1,2]
+	print Solution().removeDuplicates(inp)
