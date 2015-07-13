f1deba31f6f71a358641194649197a20bb7f525d
liangk12 <kenneth.liang12@gmail.com>
2015-07-12 22:21:14 -0400

    added solution comments for q58 length of last word. Looking to use built-in methods as rework

--- questions/q58_lengthOfLastWord.py
+++ questions/q58_lengthOfLastWord.py
@@ -17,13 +17,22 @@ class Solution(object):
     # @return {integer}
     def lengthOfLastWord(self, s):
         if not s: return 0
+        # the counter of the str length
         lenctr=0
+        # bool to determine if whitespace met
         metwhsp=False
+        # keep len value to avoid refetching
         slen=len(s)
+        # start from end of str 's'
         for idx in range(slen-1,-1,-1):
+            # if char, increment
             if s[idx]!=" ": lenctr+=1
             else:
+                # indicate whitespace met
                 if not metwhsp: metwhsp=True
+                # case when many whitespace at end of str
+                # then a set of char(s) appear
+                # eg: 'a     '
                 if metwhsp and lenctr: return lenctr
         return lenctr
 
