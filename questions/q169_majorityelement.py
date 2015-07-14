f418493640ef22277f8b1b7f42add227828ea7ac
liangk12 <kenneth.liang12@gmail.com>
2015-07-13 21:44:13 -0400

    reworking q169 majority element to cut down run time, added solution comments

--- questions/q169_majorityelement.py
+++ questions/q169_majorityelement.py
@@ -9,9 +9,15 @@ class Solution(object):
     # @param {integer[]} nums
     # @return {integer}
     def majorityElement(self, nums):
+        # dict to hold each int's count
         numsd={}
-        numslen=len(nums)
-        for idx,n in enumerate(nums):
-            numsd[n]=numsd.get(n,0)+1
-            if numsd[n]>numslen/2:
+        # get value of half of len to avoid repeat of same value
+        numsHalfLen=len(nums)/2
+        for n in nums:
+            # set in the new occurrence count
+            # 'count' variable holds the occurrence value
+            # -prevent fetching in dict; ~slow key-value retrieval
+            count=numsd[n]=numsd.get(n,0)+1
+            if count>numsHalfLen:
+                # return value if majority
                 return n
