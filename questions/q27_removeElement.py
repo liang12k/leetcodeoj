5fe2ea33d1f6e3926bed1f886546fdf97b31f0df
liangk12 <kenneth.liang12@gmail.com>
2015-07-17 22:45:35 -0400

    solved q27 remove element in place. To add solution comments

---
+++ questions/q27_removeElement.py
@@ -0,0 +1,18 @@
+'''
+Given an array and a value, remove all instances 
+of that value in place and return the new length.
+
+The order of elements can be changed.
+It doesn't matter what you leave beyond the new length.
+'''
+class Solution(object):
+    # @param {integer[]} nums
+    # @param {integer} val
+    # @return {integer}
+    def removeElement(self, nums, val):
+        numslen=len(nums)-1
+        while numslen>-1:
+            if nums[numslen]==val:
+                nums.pop(numslen)
+            numslen-=1
+        return len(nums)
