b5ba50a566dc1f015aff270a3b2a42ca1733f3d3
liangk12 <kenneth.liang12@gmail.com>
2015-07-19 23:44:34 -0400

    sample 2**5 guide on how code was derived for q50 pow(x,n)

--- questions/q50_pow_x_n.py
+++ questions/q50_pow_x_n.py
@@ -26,6 +26,27 @@ class Solution(object):
             answ = 1/answ
         return answ
 
+'''
+e.g.:
+x=2, n=5 :: (2**5)
+
+5%2==1 -> 2**1 == 2
+5/2==2 -> 2**2 == 4
+ |
+ | use this '2' value
+ V
+2%2==0 -> 2**0 == 1
+2/2==1 -> 2**1 == 2
+ |
+ | use this '1' value
+ V
+1%2==1 -> 2**1 == 2
+1/2==0 -> 2**0 == 1
+
+n has reached '0' by dividing by 2
+therefore
+2 * 4 * 1 * 2 * 2 * 1 == 32
+'''
 
 if __name__ == '__main__':
     base = 34.00515
