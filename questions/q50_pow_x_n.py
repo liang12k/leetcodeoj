71da70b8f5caf10a9875af21f133f4603070a9ad
liangk12 <kenneth.liang12@gmail.com>
2015-07-19 22:59:56 -0400

    solved q50 pow(x,n). Will add solution comments

--- questions/q50_pow_x_n.py
+++ questions/q50_pow_x_n.py
@@ -6,11 +6,17 @@ class Solution(object):
     # @param {integer} n
     # @return {float}
     def myPow(self, x, n):
-        answ=1
+        isneg = False
+        if n < 0:
+            n = 0-n
+            isneg = True
+        answ = 1
         while n > 0:
-            answ = answ*(x**(n % 2))
+            answ = answ*(x**(n%2))
             n /= 2
             answ = answ*(x**n)
+        if isneg:
+            answ = 1/answ
         return answ
 
 
@@ -18,4 +24,3 @@ if __name__ == '__main__':
     base = 34.00515
     expon = -3
     print Solution().myPow(base,expon)
-    # expecting 0.00003
