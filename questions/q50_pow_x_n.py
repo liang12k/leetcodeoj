464f5c80e446c60288fd98bd2ad35ffbced17b2f
liangk12 <kenneth.liang12@gmail.com>
2015-07-19 23:05:26 -0400

    added solution comments for q50 pow(x,n). Will add logic and sample approach in deriving code.

--- questions/q50_pow_x_n.py
+++ questions/q50_pow_x_n.py
@@ -6,15 +6,22 @@ class Solution(object):
     # @param {integer} n
     # @return {float}
     def myPow(self, x, n):
+        # flag to determine if n is negative
+        # will need to do 1/x**n later if so
         isneg = False
         if n < 0:
+            # make n positive for decrementing
             n = 0-n
             isneg = True
+        # base is always 1; to the 0th power
         answ = 1
         while n > 0:
-            answ = answ*(x**(n%2))
+            # first modulus
+            answ = answ*(x**(n % 2))
+            # then decrement by dividing by 2
             n /= 2
             answ = answ*(x**n)
+        # if n was originally negative, need to flip into 1/answ
         if isneg:
             answ = 1/answ
         return answ
@@ -23,4 +30,4 @@ class Solution(object):
 if __name__ == '__main__':
     base = 34.00515
     expon = -3
-    print Solution().myPow(base,expon)
+    print Solution().myPow(base, expon)
