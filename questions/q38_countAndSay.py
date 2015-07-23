bd895cd104d2508a4cf22d5e7393ec02709d611a
liangk12 <kenneth.liang12@gmail.com>
2015-07-23 08:02:45 -0400

    helpful clarification input n is the index, function returns the nth element for q38 count and say

--- questions/q38_countAndSay.py
+++ questions/q38_countAndSay.py
@@ -9,6 +9,25 @@ beginning as follows:
 Given an integer n, generate the nth sequence.
 
 Note: The sequence of integers will be represented as a string.
+
+---
+https://leetcode.com/discuss/40007/if-i-input-2-the-expected-integer-is-11-%EF%BC%9F
+
+The task says - generate the sequence.
+the sequence is made the way when item N is build based on N-1
+so item 1 - always 1 as a start point.
+the next item you build pairs based on the previous item
+following the riles rules ...
+
+and here is how the sequence is built (added . and : to separate pairs)
+
+1 -> 1 (start point)
+2 -> 1.1 (based on previous element which is 1)
+3 -> 2.1 (where it says we have 2 count of digit 1)
+4 -> 1.2:1.1 (where it says we have 1 count of digit 2
+              and 1 count of digit 1)
+5 -> 1.1:1.2:2.1 (1 count of 1, 1 count of 2, 2 count of 1)
+etc.
 '''
 class Solution(object):
     # @param {integer} n
