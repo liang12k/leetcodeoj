ae446ff71a276ab180a829dd5500d7771d206f5c
liangk12 <kenneth.liang12@gmail.com>
2015-07-26 14:33:02 -0400

    added solution comments and approach for q38 count and say

--- questions/q38_countAndSay.py
+++ questions/q38_countAndSay.py
@@ -28,32 +28,61 @@ and here is how the sequence is built (added . and : to separate pairs)
               and 1 count of digit 1)
 5 -> 1.1:1.2:2.1 (1 count of 1, 1 count of 2, 2 count of 1)
 etc.
+
+pattern is:
+1  :  1
+2  :  11
+3  :  21
+4  :  1211
+5  :  111221
+6  :  312211
+7  :  13112221
+8  :  1113213211
+9  :  31131211131221
 '''
 class Solution(object):
     # @param {integer} n
     # @return {string}
     def countAndSay(self, n):
+        # base case
         if n == 1:
             return "1"
+        # initial str
         sstr = "1"
+        # keep expanding num str for n iterations
+        # *note*: n==1 handled at base case
         while n > 1:
+            # ans holds the latest num str at n-iteration
+            # need to reset for every iteration to get latest
+            # -see end of while-loop; take ans if while-loop ends
             ans = ""
+            # curr num to keep count @
             curr = sstr[0]
+            # initial counter
             ctr = 0
+            # iterate thru each num str element
             for num in sstr:
                 num = str(num)
                 if num == curr:
                     ctr += 1
                 else:
+                    # set ans to latest
+                    # *note*: going from left to right, append as such
                     ans += str(ctr)+curr
+                    # set curr to latest num, reset ctr
                     curr = num
                     ctr = 1
+            # exiting for-loop; still remains the latest curr & its ctr
+            # append it accordinglt to ans
+            # *note* if end of while-loop, take ans and return
             ans += str(ctr)+curr
+            # set ans as latest sstr for next iteration if needed
             sstr = ans
+            # decrement n for while-loop
             n -= 1
         return ans
 
 
 if __name__ == '__main__':
     for n in range(1, 10):
-        print n, Solution().countAndSay(n)
+        print n, " : ", Solution().countAndSay(n)
