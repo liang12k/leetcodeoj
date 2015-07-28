e81f5055f35374794c2bb3806dfac8fb1e7c4e16
liangk12 <kenneth.liang12@gmail.com>
2015-07-28 00:02:17 -0400

    added solution comments for q66 add one

--- questions/q66_addone.py
+++ questions/q66_addone.py
@@ -9,11 +9,17 @@ class Solution(object):
     # @param {integer[]} digits
     # @return {integer[]}
     def plusOne(self, digits):
+        # base case: at least 1 (if input is 0)
         totalsum = 1
+        # reversed() returns a generator
         for idx, elem in enumerate(reversed(digits)):
+            # get total sum using modulus
+            # use idx as the exponent at 10th place
             totalsum += (elem*(10**idx))
+        # reuse the digits variable
         digits = []
         while totalsum > 0:
+            # insert is faster than list concating
             digits.insert(0, totalsum % 10)
             totalsum /= 10
         return digits
