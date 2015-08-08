13a336a25066b029344600f5955ddec04d260591
liangk12 <kenneth.liang12@gmail.com>
2015-08-08 15:32:24 -0400

    solution comments for q118 pascal triangle

--- questions/q118_pascaltriangle.py
+++ questions/q118_pascaltriangle.py
@@ -16,15 +16,23 @@ class Solution(object):
     # @param {integer} numRows
     # @return {integer[][]}
     def generate(self, numRows):
+        # base of pascal triangle for numRows in [1,2]
         pasc = [[1], [1, 1]]
+        # use pasc as is for 0,1,2
+        # *note*: pasc[:0] == []
         if numRows < 2:
             return pasc[:numRows]
+        # begin getting pascal values from numRows>2
         numRows -= 2
         while numRows:
             nextpasc = []
             currpasc = pasc[-1]
+            # iterate thru latest pascal value list
+            # end when the idx+1 goes beyond the len range
             for idx in range(len(currpasc)-1):
                 nextpasc.append(currpasc[idx]+currpasc[idx+1])
+            # all pascal values begin,end with 1
+            # append generated pascal value into pasc list
             pasc.append([1]+nextpasc+[1])
             numRows -= 1
         return pasc
