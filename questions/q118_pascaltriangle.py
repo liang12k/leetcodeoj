1bed7160838fe3c788a209a8758cc796e60b43b6
liangk12 <kenneth.liang12@gmail.com>
2015-08-07 22:36:27 -0400

    solved q118 pascal triangle w reworking iterating thru latest pascal row. Need to add solution comments. Runs @ 52ms per leetcode

--- questions/q118_pascaltriangle.py
+++ questions/q118_pascaltriangle.py
@@ -16,16 +16,17 @@ class Solution(object):
     # @param {integer} numRows
     # @return {integer[][]}
     def generate(self, numRows):
-        pasc=[[1],[1,1]]
-        numRows-=2
+        if numRows < 1:
+            return []
+        pasc = [[1], [1, 1]]
+        if numRows == 1:
+            return pasc[:1]
+        numRows -= 2
         while numRows:
-            nextpasc=[1]
-            currpasc=pasc[-1]
-            idx=0
-            while (idx+1)<len(currpasc):
+            nextpasc = []
+            currpasc = pasc[-1]
+            for idx in range(len(currpasc)-1):
                 nextpasc.append(currpasc[idx]+currpasc[idx+1])
-                idx+=1
-            nextpasc.append(1)
-            pasc.append(nextpasc)
-            numRows-=1
-        return pasc[-1]
+            pasc.append([1]+nextpasc+[1])
+            numRows -= 1
+        return pasc
