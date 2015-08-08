274db4d86dc0a1daa5948efc94a67bc6836fe642
liangk12 <kenneth.liang12@gmail.com>
2015-08-07 22:21:48 -0400

    brute approach on pascal triangle q118 - memory limit exceeded. Need to refine

--- questions/q118_pascaltriangle.py
+++ questions/q118_pascaltriangle.py
@@ -12,3 +12,20 @@ Return
  [1,4,6,4,1]
 ]
 '''
+class Solution(object):
+    # @param {integer} numRows
+    # @return {integer[][]}
+    def generate(self, numRows):
+        pasc=[[1],[1,1]]
+        numRows-=2
+        while numRows:
+            nextpasc=[1]
+            currpasc=pasc[-1]
+            idx=0
+            while (idx+1)<len(currpasc):
+                nextpasc.append(currpasc[idx]+currpasc[idx+1])
+                idx+=1
+            nextpasc.append(1)
+            pasc.append(nextpasc)
+            numRows-=1
+        return pasc[-1]
