c78ddae0e2b362d66cfc16c339cca72d8e46fcfb
liangk12 <kenneth.liang12@gmail.com>
2015-08-10 23:25:38 -0400

    renamed vars in q206 reverse linked list -- leetcode runtime actually 64ms w/o the comment for base case. Still need to add solution comments

--- questions/q206_reverseLinkedList.py
+++ questions/q206_reverseLinkedList.py
@@ -2,22 +2,22 @@
 Reverse a singly linked list.
 '''
 # Definition for singly-linked list.
-class ListNode:
+class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
 
-class Solution:
+class Solution(object):
     # @param {ListNode} head
     # @return {ListNode}
     def reverseList(self, head):
         if not head:
             return []
-        newLinkedList = ListNode(0)
-        newLinkedListPtr = newLinkedList
+        new_linked_list = ListNode(0)
+        new_linked_list_pointer = new_linked_list
         while head:
             node = ListNode(head.val)
-            node.next = newLinkedListPtr.next
-            newLinkedListPtr.next = node
+            node.next = new_linked_list_pointer.next
+            new_linked_list_pointer.next = node
             head = head.next
-        return newLinkedList.next
+        return new_linked_list.next
