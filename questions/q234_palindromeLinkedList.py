122ef42237c60d2b6fe7d7f5a6016d1268a7115e
liangk12 <kenneth.liang12@gmail.com>
2015-08-12 08:14:11 -0400

    solution comments for q234 palindrome linkedlist

--- questions/q234_palindromeLinkedList.py
+++ questions/q234_palindromeLinkedList.py
@@ -14,10 +14,16 @@ class Solution(object):
     # @param {ListNode} head
     # @return {boolean}
     def isPalindrome(self, head):
+        # list to hold the node values
+        # *note* making a string isn't a good idea
+        #        ex. input [-1,-1] is a palindrome
+        #            but checking it as a str fails
         linkedlistStr = []
         while head:
             linkedlistStr.append(head.val)
             head = head.next
+        # return bool on list comparison
+        # idea: avoid [::-1] as it uses addt'l space
         return linkedlistStr == linkedlistStr[::-1]
 
 # leetcode states 172ms
