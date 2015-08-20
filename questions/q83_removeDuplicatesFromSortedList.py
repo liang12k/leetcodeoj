a2c701396912e77e99b81c6ee8457543bfb45475
liangk12 <kenneth.liang12@gmail.com>
2015-08-19 21:43:43 -0400

    solution comments and approach of q83.

--- questions/q83_removeDuplicatesFromSortedList.py
+++ questions/q83_removeDuplicatesFromSortedList.py
@@ -16,16 +16,27 @@ class Solution(object):
     # @param {ListNode} head
     # @return {ListNode}
     def deleteDuplicates(self, head):
-        uniqlist=ListNode(0)
-        uniqlistPtr=uniqlist
-        currval=None
+        # new linkedlist w dummy value 0
+        # and create new pointer for it
+        uniqlist = ListNode(0)
+        uniqlistPtr = uniqlist
+        # var to determine the latest value
+        # *note* helps w sorted list, dict needed for random vals
+        currval = None
         while head:
-            if head.val!=currval:
-                currval=head.val
-                uniqnode=ListNode(currval)
-                uniqlistPtr.next=uniqnode
-                uniqlistPtr=uniqnode
-            head=head.next
+            if head.val != currval:
+                # take latest val
+                currval = head.val
+                # new node of unique val
+                uniqnode = ListNode(currval)
+                # point the new linkedlist ptr to this new node
+                uniqlistPtr.next = uniqnode
+                # assign the ptr to new node as this will
+                # have its .next assigned later
+                uniqlistPtr = uniqnode
+            # nonetheless, traverse thru the input linkedlist
+            head = head.next
+        # return the linkedlist after the dummy value 0
         return uniqlist.next
 
 # 88ms per leetcode
