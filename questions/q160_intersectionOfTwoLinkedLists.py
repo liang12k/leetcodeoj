d455fce4d83de68643907c03056809c75a282828
liangk12 <kenneth.liang12@gmail.com>
2015-08-23 17:46:25 -0400

    solution example comment block, will add in-code solution comments for q160

--- questions/q160_intersectionOfTwoLinkedLists.py
+++ questions/q160_intersectionOfTwoLinkedLists.py
@@ -1,6 +1,6 @@
 '''
 Write a program to find the node at which the intersection of
-two singly linked lists begins.
+two singly linked lists begins. (Y-linked list)
 
 A:          a1 -> a2
                    |
@@ -31,8 +31,54 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
+        # approach:
+        #     having 2 linked lists, pointers for each list
+        #     let each pointer go thru their respective list
+        #     for the shorter list, its pointer will change (cross)
+        #     over to the other list
+        #     continuing the traversal, the other pointer will reach its
+        #     end of list and cross over to the other list
+        #     at this point, both pointers will begin at the 'same index'
+        #     this traversal will actually determine if there's an
+        #     intersection between the 2 lists (Y-linked list)
+        #     **note**
+        #         an intersection here means both lists merge at a certain
+        #         node, hence Y-shaped linked list
+        #     
+        #     eg:
+        #         len headA > len headB
+        #         therefore, after ptrB crosses to headA, ptrA crosses to headB
+        #         this means ptrA,ptrB begin at equivalent index as if both
+        #         linked lists have same len
+        #         
+        #         headA: 1->3->4->5->x
+        #         headB: 2->4->x
+        #         
+        #         ptrA : 1
+        #         ptrB : 2
+        #         
+        #         ptrA : 3
+        #         ptrB : 4
+        #         
+        #         ptrA : 5
+        #         ptrB : x
+        #         
+        #         ptrA : x
+        #         ptrB : 1 -> goes to headA, bool flag set for the 'cross'
+        #         
+        #         **note**
+        #             the real traversal begins for finding intersection
+        #             as ptrB begin at an index equivalent to headB len
+        #         ptrA : 2 -> goes to headB, bool flag set for the 'cross'
+        #         ptrB : 3
+        #         
+        #         ptrA : 4
+        #         ptrB : 4
+        # 
         # base case: blank input(s), ignore
         if not headA or not headB: return None
+        # set the pointers for headA, headB
+        # and bool flag when either pointer changes lists
         ptrA = headA
         isChangedA = False
         ptrB = headB
