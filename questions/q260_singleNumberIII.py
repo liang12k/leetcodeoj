887084a235b8122fb3170ceeb65a55cfb0cf5f76
liangk12 <kenneth.liang12@gmail.com>
2015-08-26 21:57:06 -0400

    solution for q260, not in constant space complexity! need to learn of operator method and bits

--- questions/q260_singleNumberIII.py
+++ questions/q260_singleNumberIII.py
@@ -14,3 +14,18 @@ So in the above example, [5, 3] is also correct.
 Your algorithm should run in linear runtime complexity.
 Could you implement it using only constant space complexity?
 '''
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        numsdict={}
+        for n in nums:
+            numsdict[n]=numsdict.get(n,0)+1
+        nums=[]
+        for k,v in numsdict.iteritems():
+            if v==1: nums.append(k)
+        return nums
+
+# 64ms per leetcode
