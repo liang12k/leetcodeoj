e3316278615bd34e7d3a3a906b4c56b2474fd310
liangk12 <kenneth.liang12@gmail.com>
2015-11-19 21:01:18 -0500

    alt fibonacci, shorter, still 'shifting' values a,b=b,a+b

--- questions/fibonacci_oN.py
+++ questions/fibonacci_oN.py
@@ -14,3 +14,10 @@ def get_fibonacci_num(n):
         fib2 = sum
         n -= 1
     return sum
+
+def fibonacci(n):
+    a, b = 0, 1
+    while n>0:
+        a, b = b, a+b
+        n -= 1
+    return a
