ede8133c8671a5ace70174170dc11a763851a8ce
liangk12 <kenneth.liang12@gmail.com>
2015-12-06 16:42:35 -0500

    q70 climbing stairs w comments added

--- questions/q70_climbing_stairs.py
+++ questions/q70_climbing_stairs.py
@@ -11,9 +11,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        # this is fib question
+        # but the initial starting numbers are 1,1
+        # because w 1 stair, min of 1 possibility of 1 step
+        #
+        # 1, 1, 2, 3, 5, 8, 13, 21, ...
+        # w 3 stair-steps, there are 2 possibilities
+        # w 6 '' '' '' '', '' '' ''  8 '' ''
         a=1
         b=1
         while n:
             a,b=b,a+b # fib swap
             n-=1
-        return a
+        return a # return current
