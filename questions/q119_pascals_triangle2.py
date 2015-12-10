656ed7ce0f1fa34b8fd2acdfc259f2fc6744f071
liangk12 <kenneth.liang12@gmail.com>
2015-12-09 22:03:56 -0500

    q119 pascals triangle2 w comments added

--- questions/q119_pascals_triangle2.py
+++ questions/q119_pascals_triangle2.py
@@ -11,10 +11,15 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
+        # determine base cases
         if -1<rowIndex<4: return [[1],[1,1],[1,2,1],[1,3,3,1]][rowIndex]
         pascalTriNums=[1,3,3,1] # from here, begins @ [1,3,3,1]
-        rowIndex-=3
+        rowIndex-=3 # idx already @ 3 b/c starting at [1,3,3,1]
+        # keep iterating until end of desired level reached
+        # using rowIndex
         while rowIndex:
+            # each row starts and ends w a 1
+            # everything in betw is a sum of all vals from previous level
             pascalTriNums=[1]+[pascalTriNums[x]+pascalTriNums[x+1] for x in xrange(len(pascalTriNums)-1)]+[1]
             rowIndex-=1
         return pascalTriNums
