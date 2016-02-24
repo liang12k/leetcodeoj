997a70c037aa26dce077f43fd3a18e1bfdce78ef
liangk12 <kenneth.liang12@gmail.com>
2016-02-23 23:44:28 -0500

    q326 - power of three, alternate solution using the math module's log method to determine the difference of precision of checking if n is a power of 3

--- questions/q326_powerOfThree.py
+++ questions/q326_powerOfThree.py
@@ -5,12 +5,16 @@ it is a power of three.
 Follow up:
 Could you do it without using any loop / recursion?
 '''
+from math import log
+
+
 class Solution(object):
     def isPowerOfThree(self, n):
         """
         :type n: int
         :rtype: bool
         """
+        ## # is this a valid approach? using a while-loop
         if n<1:
             # avoid neg nums, and 0 isn't do able
             return False
@@ -23,3 +27,15 @@ class Solution(object):
             n /= 3
         # default n is a power of 3 after finish breaking down
         return True
+    
+    def isPowerOfThree_alt(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # handle base case n can't be powered to 0
+        # this uses the log approach to get the cube of n
+        # needs to handle different precision as
+        # the log will be a float against a rounded down log
+        return n>0 and abs(log(n,3) - round(log(n,3))) < 1e-10
+    
